1ee8cc46005cf27f22e6ea0549a0e19051d562c7
Behzad-Moradi-Junior-Role <behzad.moradi.kut@gmail.com>
2026-07-31 14:26:51 +1000

    ML modles comparison completed

---
+++ Utils/train_test_models.py
@@ -0,0 +1,241 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import classification_report
+from xgboost import XGBClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from collections import defaultdict
+import pickle
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import hamming_loss, accuracy_score, precision_score, recall_score, f1_score, jaccard_score, classification_report, multilabel_confusion_matrix
+from scipy.stats import wilcoxon
+import sqlite3
+from collections import defaultdict
+
+################################# Computing Multiclass Labels #################################    
+def multi_class_labeling(num_fun_bbob, num_fun_smd, num_inst, opty_portfolio_results_bbob, opty_portfolio_results_smd, ll_solver_list):
+    alpha = 0.05
+    y_multi_bbob = np.zeros((num_fun_bbob*num_inst, len(ll_solver_list)), dtype=int)
+    y_multi_smd = np.zeros((num_fun_smd*num_inst, len(ll_solver_list)), dtype=int)
+    
+    for p in range(num_fun_bbob*num_inst):
+        medians = np.median(opty_portfolio_results_bbob[p], axis=0)
+        best_idx = np.argmin(medians)
+        best_runs = opty_portfolio_results_bbob[p, :, best_idx]
+        y_multi_bbob[p, best_idx] = 1
+
+        for s in range(len(ll_solver_list)):
+            if s == best_idx:
+                continue
+            runs = opty_portfolio_results_bbob[p, :, s]
+            if np.array_equal(best_runs, runs):
+                pvalue = 1.0
+            else:
+                _, pvalue = wilcoxon(best_runs, runs)
+                if pvalue > alpha:
+                    y_multi_bbob[p, s] = 1
+                    
+    for p in range(num_fun_smd*num_inst):
+        medians = np.median(opty_portfolio_results_smd[p], axis=0)
+        best_idx = np.argmin(medians)
+        best_runs = opty_portfolio_results_smd[p, :, best_idx]
+        y_multi_smd[p, best_idx] = 1
+
+        for s in range(len(ll_solver_list)):
+            if s == best_idx:
+                continue
+            runs = opty_portfolio_results_smd[p, :, s]
+            if np.array_equal(best_runs, runs):
+                pvalue = 1.0
+            else:
+                _, pvalue = wilcoxon(best_runs, runs)
+                if pvalue > alpha:
+                    y_multi_smd[p, s] = 1
+    
+    return y_multi_bbob, y_multi_smd
+
+###########################################################################################   
+###########################################################################################   
+###########################################################################################
+
+def train_test_models(num_fun_bbob, num_fun_smd, num_inst, dim_bbob, db_dir, src_dir, res_dir):
+    conn = sqlite3.connect(db_dir)
+    cur = conn.cursor()
+    
+    cur.execute('''SELECT fv.instance_id, fv.feature_value FROM feature_values fv
+                   JOIN problem_instances pi ON pi.instance_id = fv.instance_id
+                   JOIN problem_configs pc ON pc.config_id = pi.config_id
+                   JOIN problems p ON p.problem_id = pc.problem_id
+                   JOIN suites s ON s.suite_id = p.suite_id
+                   WHERE s.suite_name == 'BBOB'
+                   ORDER BY fv.instance_id, fv.feature_id
+                   ''')
+    
+    feature_values_bbob = cur.fetchall()
+    
+    groups = defaultdict(list)
+    for instance_id, value in feature_values_bbob:
+        groups[instance_id].append(value)
+    instance_ids = sorted(groups.keys())
+    reduced_sel_ela_features_bbob = pd.DataFrame([groups[i] for i in instance_ids], dtype=float)
+
+    cur.execute('''SELECT fv.instance_id, fv.feature_value FROM feature_values fv
+                   JOIN problem_instances pi ON pi.instance_id = fv.instance_id
+                   JOIN problem_configs pc ON pc.config_id = pi.config_id
+                   JOIN problems p ON p.problem_id = pc.problem_id
+                   JOIN suites s ON s.suite_id = p.suite_id
+                   WHERE s.suite_name == 'SMD'
+                   ORDER BY fv.instance_id, fv.feature_id
+                   ''')
+    
+    feature_values_smd = cur.fetchall()
+    
+    groups = defaultdict(list)
+    for instance_id, value in feature_values_smd:
+        groups[instance_id].append(value)
+    instance_ids = sorted(groups.keys())
+    reduced_sel_ela_features_smd = pd.DataFrame([groups[i] for i in instance_ids], dtype=float)
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    #reduced_sel_ela_features_bbob = pd.read_csv(f'{src_dir}/ela_bbob_processed_{dim_bbob}d.csv')
+    #reduced_sel_ela_features_smd = pd.read_csv(f'{src_dir}/ela_smd_processed_{dim_bbob}d.csv')
+    
+    reduced_sel_ela_features_bbob_mean = reduced_sel_ela_features_bbob.mean()
+    reduced_sel_ela_features_smd.fillna(reduced_sel_ela_features_bbob_mean, inplace=True)
+    
+    scaler = MinMaxScaler()
+    scaled_reduced_sel_ela_features_bbob = scaler.fit_transform(reduced_sel_ela_features_bbob.values)
+    scaled_reduced_sel_ela_features_smd = scaler.transform(reduced_sel_ela_features_smd.values)
+    
+    y_multi_bbob = np.load(f'{src_dir}/y_multi_bbob_{dim_bbob}d.npy')
+    y_multi_smd = np.load(f'{src_dir}/y_multi_smd_{dim_bbob}d.npy')
+    
+    models = {
+                "Dummy": OneVsRestClassifier(DummyClassifier()),
+                "Random Uniform": OneVsRestClassifier(DummyClassifier(strategy="uniform", random_state=1)),
+                "SVM": OneVsRestClassifier(SVC()),
+                "XGBoost": OneVsRestClassifier(XGBClassifier(random_state=1)),
+                "Random Forest": OneVsRestClassifier(RandomForestClassifier(random_state=1)),
+                "KNN": OneVsRestClassifier(KNeighborsClassifier()),
+                "MLP": OneVsRestClassifier(MLPClassifier(random_state=1))
+            }
+
+################## Leave One Instance Out ################
+    mlc_loio_results = {}
+
+    test_ratio = 0.1
+    test_num_inst = int(test_ratio * num_inst)
+    all_indices = np.arange(num_fun_bbob * num_inst)
+    base = np.arange(num_fun_bbob)*num_inst  
+    test_range = np.arange(test_num_inst)
+    
+    for i in range(int(1/test_ratio)): 
+        
+        test_indices = (base[:, None]+i*test_num_inst + test_range[None, :]).flatten()
+        train_indices = np.setdiff1d(all_indices, test_indices)
+
+        train_X = reduced_sel_ela_features_bbob.iloc[train_indices]
+        train_y = y_multi_bbob[train_indices]
+        test_X = reduced_sel_ela_features_bbob.iloc[test_indices]
+        test_y = y_multi_bbob[test_indices]
+
+        scaler = MinMaxScaler()
+        train_X = scaler.fit_transform(train_X.values)
+        test_X = scaler.transform(test_X.values)
+
+        mlc_loio_results[i] = {}
+
+        for name, model in models.items():
+            model.fit(train_X, train_y)
+            y_pred = model.predict(test_X)
+
+            result = {
+                        "Subset Accuracy": accuracy_score(test_y, y_pred),
+                        "Hamming Score": 1 - hamming_loss(test_y, y_pred),
+                        "Micro Precision": precision_score(test_y, y_pred, average='micro', zero_division=0),
+                        "Micro Recall": recall_score(test_y, y_pred, average='micro', zero_division=0),
+                        "Micro F1": f1_score(test_y, y_pred, average='micro', zero_division=0),
+                        "Macro F1": f1_score(test_y, y_pred, average='macro', zero_division=0),
+                        "Jaccard Score": jaccard_score(test_y, y_pred, average='samples', zero_division=0),
+                        "Classification Report": classification_report(test_y, y_pred, output_dict=True, zero_division=0)
+                    }
+            
+            mlc_loio_results[i][name] = result
+
+    with open(f"{res_dir}/mlc_loio_results.pkl", "wb") as f:
+        pickle.dump(mlc_loio_results, f)
+    
+################## Leave One Problem Out ################
+    mlc_lopo_results = {}
+
+    for i in range(num_fun_bbob):
+        all_indices = np.arange(num_fun_bbob*num_inst)
+        test_indices = np.arange(i*num_inst, (i+1)*num_inst)
+        train_indices = np.setdiff1d(all_indices, test_indices)
+
+        train_X = reduced_sel_ela_features_bbob.iloc[train_indices]
+        train_y = y_multi_bbob[train_indices]
+        test_X = reduced_sel_ela_features_bbob.iloc[test_indices]
+        test_y = y_multi_bbob[test_indices]
+
+        scaler = MinMaxScaler()
+        train_X = scaler.fit_transform(train_X.values)
+        test_X = scaler.transform(test_X.values)
+
+        mlc_lopo_results[i] = {}
+        
+        for name, model in models.items():
+            model.fit(train_X, train_y)
+            y_pred = model.predict(test_X)
+
+            result = {
+                        "Subset Accuracy": accuracy_score(test_y, y_pred),
+                        "Hamming Score": 1 - hamming_loss(test_y, y_pred),
+                        "Micro Precision": precision_score(test_y, y_pred, average='micro', zero_division=0),
+                        "Micro Recall": recall_score(test_y, y_pred, average='micro', zero_division=0),
+                        "Micro F1": f1_score(test_y, y_pred, average='micro', zero_division=0),
+                        "Macro F1": f1_score(test_y, y_pred, average='macro', zero_division=0),
+                        "Jaccard Score": jaccard_score(test_y, y_pred, average='samples', zero_division=0),
+                        "Classification Report": classification_report(test_y, y_pred, output_dict=True, zero_division=0)
+                    }
+
+            mlc_lopo_results[i][name] = result
+
+    with open(f"{res_dir}/mlc_lopo_results.pkl", "wb") as f:
+        pickle.dump(mlc_lopo_results, f)
+
+################## Leave One Suite Out ################
+    mlc_loso_results = {}
+
+    for i in range(num_fun_smd):
+        mlc_loso_results[i] = {}   
+        for name, model in models.items():
+                model.fit(scaled_reduced_sel_ela_features_bbob, y_multi_bbob)
+                y_pred = model.predict(scaled_reduced_sel_ela_features_smd[i*num_inst:(i+1)*num_inst])
+
+                result = {
+                            "Subset Accuracy": accuracy_score(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred),
+                            "Hamming Score": 1 - hamming_loss(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred),
+                            "Micro Precision": precision_score(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred, average='micro', zero_division=0),
+                            "Micro Recall": recall_score(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred, average='micro', zero_division=0),
+                            "Micro F1": f1_score(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred, average='micro', zero_division=0),
+                            "Macro F1": f1_score(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred, average='macro', zero_division=0),
+                            "Jaccard Score": jaccard_score(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred, average='samples', zero_division=0),
+                            "Classification Report": classification_report(y_multi_smd[i*num_inst:(i+1)*num_inst], y_pred, output_dict=True, zero_division=0)
+                        }
+
+                mlc_loso_results[i][name] = result
+
+    with open(f"{res_dir}/mlc_loso_results.pkl", "wb") as f:
+        pickle.dump(mlc_loso_results, f) 
+        
+    return mlc_loio_results, mlc_lopo_results, mlc_loso_results
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Utils.create_database import create_database_schema, populate_database
+from Utils.train_test_models import train_test_models
+from Utils.visualisation import visualisation
 import sqlite3
 
 ELA_FEATURE_NAMES = [
@@ -65,10 +67,7 @@ def main():
     NUM_SAMPLE_POINTS = NUM_SAMPLE_COEFF*PROB_DIM
     OPT_BUDGET = 1000*PROB_DIM
     NUM_RUN = 51
-    
     BOOTSTRAP_RATIO = 0.8
-        
-    
     BBOB_LOWER_BOUND = -5.0
     BBOB_UPPER_BOUND = 5.0
     
@@ -79,14 +78,17 @@ def main():
     SAMPLING_SET_LIST = [{'sampling_method': 'Sobol', 'num_points': NUM_SAMPLE_POINTS, 'problem_dim': PROB_DIM, 'random_seed': 1}]
     
     DATABASE_DIR ='DataBase/optialgosel.db'
+    SRC_DIR = 'Resources'
+    RES_DIR = 'Results'
     
     ALGORITHM_LIST = [{'algorithm_name': 'CMAES', 'algorithm_description': 'Covariance Matrix Adaptation Evolution Strategy'}, {'algorithm_name': 'ES', 'algorithm_description': 'Evolution Strategy'}, {'algorithm_name': 'GA', 'algorithm_description': 'Genetic Algorithm'}, {'algorithm_name': 'DE', 'algorithm_description': 'Differential Evolution'}, {'algorithm_name': 'PSO', 'algorithm_description': 'Particle Swarm Optimization'}, {'algorithm_name': 'NM', 'algorithm_description': 'Nelder-Mead'}, {'algorithm_name': 'PS', 'algorithm_description': 'Particle Swarm'}]
      
     PERFORMANCE_METRIC_LIST = [{'metric_name': 'Optimal Solution', 'metric_description': 'Optimal Solution'}]
 
-    #create_database_schema(DATABASE_DIR)
+    create_database_schema(DATABASE_DIR)
     populate_database(SUITE_LIST, PROB_DIM, UL_DIM_SMD, BBOB_LOWER_BOUND, BBOB_UPPER_BOUND, SAMPLING_SET_LIST, ALGORITHM_LIST, PERFORMANCE_METRIC_LIST, OPT_BUDGET, NUM_RUN, ELA_FEATURE_NAMES, NUM_SEED, BOOTSTRAP_RATIO, DATABASE_DIR)
-       
+    train_test_models(NUM_FUN_BBOB, NUM_FUN_SMD, NUM_INST, PROB_DIM, DATABASE_DIR, SRC_DIR, RES_DIR)
+    visualisation(NUM_FUN_BBOB, NUM_FUN_SMD, SRC_DIR, RES_DIR) 
     return
 
 if __name__ == "__main__":
